385aa11dfcd2c3d6034bb020a41e9a3794109d9c
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-06-06 14:43:49 -0700

    solution to lesson 1

--- 01_ruby_fundamentals/lib/dog_walker_cli.rb
+++ 01_ruby_fundamentals/lib/dog_walker_cli.rb
@@ -8,14 +8,27 @@ end
 #   2. Add a dog
 # At any time, type "menu" to see these options again or "exit" to
 # leave the program
+def print_menu_options
+  puts "  1. List all dogs"
+  puts "  2. Add a dog"
+  puts 'At any time, type "exit" to leave the program'
+end
 
 
 
-
-# define a method `ask_for_choice` which prompts the user for input
+# define a method `ask_for_input` which prompts the user for input
 # if the user types "exit" we'll print a message thanking them
 # for using the CLI and invoke exit to terminate the program
 # otherwise, return whatever the user typed in
+def ask_for_input
+  input = gets.chomp
+  if input == "exit"
+    puts "Thank you for using the Dog Walker CLI! Have a nice day :)"
+    exit
+  else
+    input
+  end
+end
 
 
 
@@ -29,12 +42,26 @@ Lennon Snow
   Image URL: https://res.cloudinary.com/dnocv6uwb/image/upload/v1609370267/dakota-and-lennon-square-compressed_hoenfo.jpg
 
 =end
+def print_dog(dog_hash)
+  puts
+  puts "#{dog_hash[:name]}".green
+  puts "  Age: #{dog_hash[:age]}"
+  puts "  Breed: #{dog_hash[:breed]}"
+  puts "  Image URL: #{dog_hash[:image_url]}"
+  puts
+end
 
 
 
 
 # define a method `list_dogs` that will iterate over an array of
 # dog hashes and call print_dog on each one.
+def list_dogs(dogs)
+  dogs.each do |dog_hash|
+    print_dog(dog_hash)
+  end
+  nil
+end
 
 
 
@@ -45,13 +72,40 @@ Lennon Snow
 # Take this information and put it into a hash
 # add the hash to the dogs array passed as an argument
 # print the newly added dog
-
+def add_dog(dogs)
+  print "What's the dog's name? "
+  name = ask_for_input
+  print "What's the dog's age? "
+  age = ask_for_input
+  print "What's the dog's breed? "
+  breed = ask_for_input
+  print "What's the dog's image_url? "
+  image_url = ask_for_input
+  dog_hash = {
+    name: name,	
+    age: age,
+    breed:	breed,
+    image_url: image_url
+  }
+  dogs << dog_hash
+  print_dog(dog_hash)
+end
 
 
 # define a method `handle_choice` which will take a `choice` as a 
 # parameter and handle it in the appropriate way based on the menu
 # option that was chosen
 
-
+def handle_choice(choice)
+  if choice == "1"
+    list_dogs($dogs)
+  elsif choice == "2"
+    add_dog($dogs)
+  elsif choice == "debug"
+    binding.pry
+  else
+    "Whoops! I didn't understand that, please try again!".red
+  end
+end
 
 
