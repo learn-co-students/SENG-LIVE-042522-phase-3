ece8eec1f9439d175573d318c1cfd604eb5cecbf
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2022-06-06 09:50:53 -0700

    Initialize 05_begin_intro_to_activerecord

---
+++ 05_intro_to_activerecord/demo/db/seeds.rb
@@ -0,0 +1,17 @@
+lennon = Dog.create(
+  name: "Lennon", 
+  age: "1 year", 
+  breed: "Pomeranian", 
+  image_url: "https://res.cloudinary.com/dnocv6uwb/image/upload/v1609370267/dakota-and-lennon-square-compressed_hoenfo.jpg"
+)
+olivia = Dog.create(
+  name: "Olivia",	
+  birthdate: Date.new(2018, 03, 31),
+  breed:	"Terrier",
+  image_url: "https://res.cloudinary.com/dnocv6uwb/image/upload/v1631229064/zx6CPsp_d_utkmww.webp"
+)
+
+# create a couple of walks and feedings for Lennon 
+
+
+# create a couple of walks and feedings for Memphis
